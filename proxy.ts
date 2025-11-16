5e0f1a8c1e2c723d3338e7cf286db3e4ca3aa842
Jason Congdon <jason.s.congdon@gmail.com>
2025-11-16 17:09:35 -0500

    chore: migrate from middleware to proxy convention (Next.js 14+)

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { verifyToken } from './lib/auth/jwt-edge'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
 
